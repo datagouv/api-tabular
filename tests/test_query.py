8c6fa68b6fc0a1d1f6d1f4ee289a115a9f388283
quaxsze <plum.stanislas@gmail.com>
2023-05-15 15:40:50 +0200

    Modify query to fit new needs

---
+++ tests/test_query.py
@@ -0,0 +1,55 @@
+from udata_hydra_csvapi.query import build_sql_query_string
+
+
+def test_query_build_limit():
+    query_str = 'limit=12'
+    result = build_sql_query_string(query_str)
+    assert result == 'limit=12'
+
+
+def test_query_build_offset():
+    query_str = 'offset=12'
+    result = build_sql_query_string(query_str)
+    assert result == 'offset=12'
+
+
+def test_query_build_sort_asc():
+    query_str = 'column_name__sort=asc'
+    result = build_sql_query_string(query_str)
+    assert result == 'order=column_name.asc'
+
+
+def test_query_build_sort_desc():
+    query_str = 'column_name__sort=desc'
+    result = build_sql_query_string(query_str)
+    assert result == 'order=column_name.desc'
+
+
+def test_query_build_exact():
+    query_str = 'column_name__exact=BIDULE'
+    result = build_sql_query_string(query_str)
+    assert result == 'column_name=eq.BIDULE'
+
+
+def test_query_build_contains():
+    query_str = 'column_name__contains=BIDULE'
+    result = build_sql_query_string(query_str)
+    assert result == 'column_name=like.*BIDULE*'
+
+
+def test_query_build_less():
+    query_str = 'column_name__less=12'
+    result = build_sql_query_string(query_str)
+    assert result == 'column_name=lte.12'
+
+
+def test_query_build_greater():
+    query_str = 'column_name__greater=12'
+    result = build_sql_query_string(query_str)
+    assert result == 'column_name=gte.12'
+
+
+def test_query_build_multiple():
+    query_str = 'column_name__exact=BIDULE&limit=5&offset=12'
+    result = build_sql_query_string(query_str)
+    assert result == 'column_name=eq.BIDULE&limit=5&offset=12'
--- udata_hydra_csvapi/query.py
+++ udata_hydra_csvapi/query.py
@@ -12,6 +12,36 @@ class QueryException(web.HTTPException):
         super().__init__(content_type="application/json", text=json.dumps(data))
 
 
+def build_sql_query_string(request_arg: str) -> str:
+    sql_query = ''
+    for arg in request_arg.split('&'):
+        value = arg.split('=')[1]
+        argument = arg.split('=')[0]
+        if '__' in argument:
+            comparator = argument.split('__')[1]
+            column = argument.split('__')[0]
+            if comparator == 'sort':
+                if value == 'asc':
+                    sql_query += f'order={column}.asc&'
+                elif value == 'desc':
+                    sql_query += f'order={column}.desc&'
+            elif comparator == 'exact':
+                sql_query += f'{column}=eq.{value}&'
+            elif comparator == 'contains':
+                sql_query += f'{column}=like.*{value}*&'
+            elif comparator == 'less':
+                sql_query += f'{column}=lte.{value}&'
+            elif comparator == 'greater':
+                sql_query += f'{column}=gte.{value}&'
+        elif argument == 'limit':
+            sql_query += f'{arg}&'
+        elif argument == 'offset':
+            sql_query += f'{arg}&'
+    if sql_query[-1] == '&':
+        sql_query = sql_query[:-1]
+    return sql_query
+
+
 async def get_resource(session: ClientSession, resource_id: str, columns: list):
     q = f"select={','.join(columns)}&resource_id=eq.{resource_id}&order=created_at.desc"
     url = f"{config.PG_RST_URL}/tables_index?{q}"
@@ -25,7 +55,8 @@ async def get_resource(session: ClientSession, resource_id: str, columns: list):
 
 
 async def get_resource_data(session: ClientSession, resource: dict, query_string: str):
-    async with session.get(f"{config.PG_RST_URL}/{resource['parsing_table']}?{query_string}") as res:
+    sql_query = build_sql_query_string(query_string)
+    async with session.get(f"{config.PG_RST_URL}/{resource['parsing_table']}?{sql_query}") as res:
         if not res.ok:
             raise QueryException(res.status, await res.json())
         async for chunk in res.content.iter_chunked(1024):
