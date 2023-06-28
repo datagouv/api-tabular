de5683b61e07cea141d1fb757961505fbbd8fc1a
maudetes <maudet.estelle@gmail.com>
2023-06-28 18:19:24 +0200

    Add POSTGREST_ENDPOINT config

--- udata_hydra_csvapi/__init__.py
+++ udata_hydra_csvapi/__init__.py
@@ -16,6 +16,10 @@ class Configurator:
     def configure(self):
         # load default settings
         configuration = toml.load(Path(__file__).parent / "config_default.toml")
+        if "POSTGREST_ENDPOINT" in os.environ:
+            configuration["PG_RST_URL"] = f"http://{os.getenv('POSTGREST_ENDPOINT')}"
+
+        configuration["PG_RST_URL"]
 
         # override with local settings
         local_settings = os.environ.get("CSVAPI_SETTINGS", Path.cwd() / "config.toml")
