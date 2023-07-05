d2fe5a55d7a589455a040879dc3def4e38f86dda
quaxsze <plum.stanislas@gmail.com>
2023-07-05 15:29:45 +0200

    new file metrics

--- udata_hydra_csvapi/metrics.py
+++ udata_hydra_csvapi/metrics.py
@@ -5,9 +5,8 @@ from aiohttp import web, ClientSession
 
 from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 from udata_hydra_csvapi import config
-from udata_hydra_csvapi.query import get_resource, get_resource_data
-from udata_hydra_csvapi.utils import build_sql_query_string, build_link_with_page
-from udata_hydra_csvapi.error import QueryException
+from udata_hydra_csvapi.utils import build_sql_query_string, build_link_with_page, process_total
+from udata_hydra_csvapi.error import QueryException, handle_exception
 
 routes = web.RouteTableDef()
 
@@ -18,44 +17,22 @@ sentry_sdk.init(
 )
 
 
-@routes.get(r"/api/resources/{rid}/")
-async def resource_meta(request):
-    resource_id = request.match_info["rid"]
-    resource = await get_resource(
-        request.app["csession"], resource_id, ["created_at", "url"]
-    )
-    return web.json_response(
-        {
-            "created_at": resource["created_at"],
-            "url": resource["url"],
-            "links": [
-                {
-                    "href": f"/api/resources/{resource_id}/profile/",
-                    "type": "GET",
-                    "rel": "profile",
-                },
-                {
-                    "href": f"/api/resources/{resource_id}/data/",
-                    "type": "GET",
-                    "rel": "data",
-                },
-            ],
-        }
-    )
+async def get_object_data(session: ClientSession, model: str, sql_query: str):
+    headers = {"Prefer": "count=exact"}
+    url = f"{config.PG_RST_URL}/{model}?{sql_query}"
+    async with session.get(url, headers=headers) as res:
+        if not res.ok:
+            handle_exception(
+                res.status, "Database error", await res.json(), None
+            )
+        record = await res.json()
+        total = process_total(res.headers.get("Content-Range"))
+        return record, total
 
 
-@routes.get(r"/api/resources/{rid}/profile/")
-async def resource_profile(request):
-    resource_id = request.match_info["rid"]
-    resource = await get_resource(
-        request.app["csession"], resource_id, ["profile:csv_detective"]
-    )
-    return web.json_response(resource)
-
-
-@routes.get(r"/api/resources/{rid}/data/")
+@routes.get(r"/api/{model}/data/")
 async def resource_data(request):
-    resource_id = request.match_info["rid"]
+    model = request.match_info["model"]
     query_string = request.query_string.split("&") if request.query_string else []
     page = int(request.query.get("page", "1"))
     page_size = int(request.query.get("page_size", config.PAGE_SIZE_DEFAULT))
@@ -72,11 +49,8 @@ async def resource_data(request):
     except ValueError:
         raise QueryException(400, None, "Invalid query string", "Malformed query")
 
-    resource = await get_resource(
-        request.app["csession"], resource_id, ["parsing_table"]
-    )
-    response, total = await get_resource_data(
-        request.app["csession"], resource, sql_query
+    response, total = await get_object_data(
+        request.app["csession"], model, sql_query
     )
 
     next = build_link_with_page(request.path, query_string, page + 1, page_size)
@@ -84,7 +58,6 @@ async def resource_data(request):
     body = {
         "data": response,
         "links": {
-            "profile": f"/api/resources/{resource_id}/profile/",
             "next": next if page_size + offset < total else None,
             "prev": prev if page > 1 else None,
         },
