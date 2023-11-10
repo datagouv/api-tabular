72707c8cf807f8debaf7c0d679b377bc3a042d50
maudetes <maudet.estelle@gmail.com>
2023-11-10 17:51:03 +0100

    Add SCHEME config

--- api_tabular/utils.py
+++ api_tabular/utils.py
@@ -43,15 +43,19 @@ def process_total(raw_total: str) -> int:
     return int(str_total)
 
 
+def external_url(url):
+    return f"{config.SCHEME}://{config.SERVER_NAME}{url}"
+
+
 def build_link_with_page(request: Request, query_string: str, page: int, page_size: int):
     q = [string for string in query_string if not string.startswith("page")]
     q.extend([f"page={page}", f"page_size={page_size}"])
     rebuilt_q = "&".join(q)
-    return f"{config.SERVER_NAME}{request.path}?{rebuilt_q}"
+    return external_url(f"{request.path}?{rebuilt_q}")
 
 
 def url_for(request: Request, route: str, *args, **kwargs):
     router = request.app.router
     if kwargs.pop("_external", None):
-        return f"{config.SERVER_NAME}{router[route].url_for(**kwargs)}"
+        return external_url(router[route].url_for(**kwargs))
     return router[route].url_for(**kwargs)
