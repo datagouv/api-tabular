6bb2f191e3374dc9e2f1065b219077d9950c7367
Pierlou <pierlou.ramade@data.gouv.fr>
2024-08-14 18:00:02 +0200

    feat: draft of swagger tests

---
+++ tests/test_swagger.py
@@ -0,0 +1,30 @@
+import pytest
+import csv
+import json
+import os
+
+from api_tabular.utils import external_url
+
+from .conftest import RESOURCE_ID, DATE, PGREST_ENDPOINT, TABLES_INDEX_PATTERN
+
+pytestmark = pytest.mark.asyncio
+
+
+async def test_swagger_endpoint(client, rmock):
+    rmock.get(TABLES_INDEX_PATTERN, payload=[{"profile": {"columns": {}}}])
+    res = await client.get(f"/api/resources/{RESOURCE_ID}/swagger/")
+    assert res.status == 200
+
+
+async def test_swagger_content(client, rmock):
+    with open("db/sample.csv", newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        # getting the csv-detective output in the test file
+        row, col = 1, 2
+        for idx_r, r in enumerate(spamreader):
+            if idx_r == row:
+                inspection = json.loads(r[col])
+    rmock.get(TABLES_INDEX_PATTERN, payload=[{"profile": {"columns": inspection["columns"]}}])
+    res = await client.get(f"/api/resources/{RESOURCE_ID}/swagger/")
+    assert res.status == 200
+    # NOT DONE YET
