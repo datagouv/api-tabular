9afd71f350c6b5c7ebb8b5f11036be4e120421dc
Pierlou <pierlou.ramade@data.gouv.fr>
2024-11-18 15:10:13 +0100

    refactor: adapt to new syntax bis

--- tests/test_api.py
+++ tests/test_api.py
@@ -109,7 +109,7 @@ async def test_api_resource_data_with_args_case(client, rmock):
     args = "COLUM_NAME__EXACT=BIDULE&page=1"
     rmock.get(TABLES_INDEX_PATTERN, payload=[{"__id": 1, "id": "test-id", "parsing_table": "xxx"}])
     rmock.get(
-        f"{PGREST_ENDPOINT}/xxx?COLUM_NAME=eq.BIDULE&limit=20&order=__id.asc",
+        f'{PGREST_ENDPOINT}/xxx?"COLUM_NAME"=eq.BIDULE&limit=20&order=__id.asc',
         payload={"such": "data"},
         headers={"Content-Range": "0-10/10"},
     )
@@ -180,7 +180,7 @@ async def test_api_resource_data_table_error(client, rmock):
 async def test_api_percent_encoding_arabic(client, rmock):
     rmock.get(TABLES_INDEX_PATTERN, payload=[{"__id": 1, "id": "test-id", "parsing_table": "xxx"}])
     rmock.get(
-        f"{PGREST_ENDPOINT}/xxx?%D9%85%D9%88%D8%A7%D8%B1%D8%AF=eq.%D9%85%D9%88%D8%A7%D8%B1%D8%AF&limit=20&order=__id.asc",  # noqa
+        f'{PGREST_ENDPOINT}/xxx?"%D9%85%D9%88%D8%A7%D8%B1%D8%AF"=eq.%D9%85%D9%88%D8%A7%D8%B1%D8%AF&limit=20&order=__id.asc',  # noqa
         status=200,
         payload={"such": "data"},
         headers={"Content-Range": "0-10/10"},
