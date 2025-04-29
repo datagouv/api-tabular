3eb2df59eaa390269d0059ab397a88d0bf724d0c
Pierlou <pierlou.ramade@data.gouv.fr>
2025-04-29 17:03:21 +0200

    feat: add swagger vs indexes test

--- tests/test_swagger.py
+++ tests/test_swagger.py
@@ -6,7 +6,7 @@ import yaml
 
 from api_tabular.utils import OPERATORS_DESCRIPTIONS, TYPE_POSSIBILITIES
 
-from .conftest import RESOURCE_ID, TABLES_INDEX_PATTERN
+from .conftest import RESOURCE_EXCEPTION_PATTERN, RESOURCE_ID, TABLES_INDEX_PATTERN
 
 pytestmark = pytest.mark.asyncio
 
@@ -75,3 +75,42 @@ async def test_swagger_content(client, rmock, allow_aggregation, mocker, mock_ge
                             and f"{c}__{_p}" in params  # aggregators are out
                         ):
                             raise ValueError(f"{c}__{_p} is in {output} output but should not")
+
+
+@pytest.mark.parametrize(
+    "allow_aggregation",
+    [
+        False,
+        True,
+    ],
+)
+async def test_swagger_with_indexes(rmock, client, mocker, allow_aggregation):
+    if allow_aggregation:
+        mocker.patch("api_tabular.config.ALLOW_AGGREGATION", [RESOURCE_ID])
+
+    indexed_col = ["col1", "col3"]
+    non_indexed_col = ["col2", "col4"]
+    rmock.get(
+        RESOURCE_EXCEPTION_PATTERN,
+        payload=[{"table_indexes": {c: "index" for c in indexed_col}}],
+        repeat=True,
+    )
+    rmock.get(
+        TABLES_INDEX_PATTERN,
+        payload=[{"profile": {"columns": {
+                c: {"python_type": "int", "format": "int", "score": 1.5}
+                for c in ["col1", "col2", "col3", "col4"]
+            }
+        }}],
+    )
+    res = await client.get(f"/api/resources/{RESOURCE_ID}/swagger/")
+    swagger = await res.text()
+    swagger_dict = yaml.safe_load(swagger)
+
+    for output in ["json", "csv"]:
+        params = swagger_dict["paths"][
+            f"/api/resources/{RESOURCE_ID}/data/{'' if output == 'json' else 'csv/'}"
+        ]["parameters"]
+        params = set([p["name"].split("__")[0] for p in params if "__" in p["name"]])
+        assert all(c in params for c in indexed_col)
+        assert not any(c in params for c in non_indexed_col)
