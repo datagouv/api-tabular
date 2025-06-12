ffdd448fc2dfbd4ecf9d4047644d123331852fdf
Pierlou <pierlou.ramade@data.gouv.fr>
2025-06-12 12:37:31 +0200

    set up conftest for db tests and comment all but one tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
+import csv
+from datetime import datetime, timezone
+from pathlib import Path
 import re
 
+import aiohttp
 import pytest
 import pytest_asyncio
 from aiohttp.test_utils import TestClient, TestServer
@@ -33,9 +37,11 @@ def rmock():
 
 @pytest_asyncio.fixture
 async def client():
-    app = await app_factory()
-    async with TestClient(TestServer(app)) as client:
-        yield client
+    # app = await app_factory()
+    # async with TestClient(TestServer(app)) as client:
+    #     yield client
+    async with aiohttp.ClientSession() as session:
+        yield session
 
 
 @pytest.fixture
@@ -46,3 +52,29 @@ def mock_get_resource_empty(rmock):
 @pytest.fixture
 def mock_get_not_exception(rmock):
     rmock.get(RESOURCE_EXCEPTION_PATTERN, payload=[], repeat=True)
+
+
+@pytest.fixture
+def base_url():
+    yield f"{config.SCHEME}://{config.SERVER_NAME.replace('localhost', '127.0.0.1')}/"
+
+
+def timestamptz_to_utc_iso(date_str: str) -> str:
+    """To convert the dates in TIMESTAMPTZ format (inserted into postgres) to isoformat"""
+    return datetime.fromisoformat((date_str + ":00").replace(" ", "T")).astimezone(timezone.utc).isoformat()
+
+
+@pytest.fixture
+def tables_index_rows():
+    base_directory = Path(__file__).parent.parent
+    rows = {}
+    with open(base_directory / "db/sample.csv", mode="r") as file:
+        reader = csv.reader(file)
+        columns = next(reader)
+        for row in reader:
+            row_dict = {
+                col: value if col != "created_at" else timestamptz_to_utc_iso(value)
+                for col, value in zip(columns, row)
+            }
+            rows[row_dict["resource_id"]] = row_dict
+    yield rows
