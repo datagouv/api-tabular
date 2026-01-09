f12e2f22724a31d5229ef6030c3e44bcdec9c95d
Pierlou Ramade <48205215+Pierlou@users.noreply.github.com>
2026-01-09 14:15:51 +0100

    Forbid to download a too large file (#89)

--- api_tabular/core/data.py
+++ api_tabular/core/data.py
@@ -1,27 +1,42 @@
 from typing import AsyncGenerator
 
-from aiohttp import ClientSession
+from aiohttp import ClientSession, web
+from aiohttp.web_request import Request
 
-from api_tabular.core.error import handle_exception
+from api_tabular import config
+from api_tabular.core.error import QueryException, handle_exception
 from api_tabular.core.utils import process_total
 
 
 async def stream_data(
     session: ClientSession,
+    request: Request,
     url: str,
-    batch_size: int,
     accept_format: str,
+    response_headers: dict,
 ) -> AsyncGenerator[bytes, None]:
     res = await session.head(f"{url}&limit=1&", headers={"Prefer": "count=exact"})
     if not res.ok:
         handle_exception(res.status, "Database error", await res.json(), None)
     total = process_total(res)
-    for i in range(0, total, batch_size):
-        async with session.get(
-            url=f"{url}&limit={batch_size}&offset={i}", headers={"Accept": accept_format}
-        ) as res:
-            if not res.ok:
-                handle_exception(res.status, "Database error", await res.json(), None)
-            async for chunk in res.content.iter_chunked(1024):
-                yield chunk
-            yield b"\n"
+    if total > config.BATCH_SIZE:
+        raise QueryException(
+            403,
+            None,
+            "Output is too long",
+            f"The output has more than {config.BATCH_SIZE} rows, please consider downloading the source file directly",
+        )
+    response = web.StreamResponse(headers=response_headers)
+    await response.prepare(request)
+
+    async with session.get(
+        url=f"{url}&limit={config.BATCH_SIZE}", headers={"Accept": accept_format}
+    ) as res:
+        if not res.ok:
+            handle_exception(res.status, "Database error", await res.json(), None)
+        async for chunk in res.content.iter_chunked(1024):
+            await response.write(chunk)
+        await response.write(b"\n")
+
+    await response.write_eof()
+    return response
