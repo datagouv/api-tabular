9ebd9dde47b797c1aa3be9f03e99021d783069ba
Pierlou <pierlou.ramade@data.gouv.fr>
2026-02-24 14:00:07 +0100

    feat: working OR flow

--- api_tabular/core/query.py
+++ api_tabular/core/query.py
@@ -11,19 +11,27 @@ def build_sql_query_string(
     offset: int = 0,
 ) -> str:
     sql_query = []
+    or_groups = defaultdict(list)
     aggregators = defaultdict(list)
     sorted = False
     for arg in request_arg:
         _split = arg.split("=")
+        fail = True
         # filters are expected to have the syntax `<column_name>__<operator>=<value>`
         if len(_split) == 2:
-            _filter, _sorted = add_filter(*_split)
-            if _filter:
-                sorted = sorted or _sorted
-                sql_query.append(_filter)
+            # handling when is(not)null is within an OR group
+            if _split[0].startswith("or__"):
+                # so that we go through the correct if-block
+                _split = _split + [None]
+            else:
+                _filter, _sorted = add_filter(*_split)
+                if _filter:
+                    sorted = sorted or _sorted
+                    sql_query.append(_filter)
+                fail = False
         # aggregators are expected to have the syntax `<column_name>__<operator>`
         # is(not)null also has this syntax but is a filter
-        elif len(_split) == 1:
+        if len(_split) == 1:
             if _split[0].split("__")[1] in ["isnull", "isnotnull"]:
                 _filter, _ = add_filter(_split[0], None)
                 sql_query.append(_filter)
@@ -31,8 +39,20 @@ def build_sql_query_string(
                 column, operator = add_aggregator(_split[0], indexes)
                 if column:
                     aggregators[operator].append(column)
-        else:
+            fail = False
+        # filters that are within an OR group are expected to have the syntax `or__<group>=<column_name>__<operator>=<value>`
+        if len(_split) == 3:
+            _or, *ored_filter = _split
+            split_or = _or.split("__")
+            if len(split_or) != 2 or split_or[0] != "or":
+                raise ValueError(f"The OR group `{_or}` is malformed")
+            _filter, _ = add_filter(*ored_filter, in_or=True)
+            or_groups[split_or[1]].append(_filter)
+            fail = False
+        if fail:
             raise ValueError(f"argument '{arg}' could not be parsed")
+    for group_filters in or_groups.values():
+        sql_query.append(f"or=({','.join(group_filters)})")
     if aggregators:
         if resource_id and not is_aggregation_allowed(resource_id):
             raise PermissionError(
@@ -72,39 +92,52 @@ def get_column_and_operator(argument: str) -> tuple[str, str]:
     return column, normalized_comparator
 
 
-def add_filter(argument: str, value: str | None) -> tuple[str | None, bool]:
+def add_filter(
+    argument: str,
+    value: str | None,
+    *,
+    in_or: bool = False,
+) -> tuple[str | None, bool]:
     if argument in ["page", "page_size"]:  # processed differently
+        if in_or:
+            raise ValueError(f"Argument `{argument}` can't be set in an OR group")
         return None, False
     if argument == "columns":
+        if in_or:
+            raise ValueError(f"Argument `{argument}` can't be set in an OR group")
         return f"select={value}", False
     if "__" in argument:
         column, normalized_comparator = get_column_and_operator(argument)
+        # when encapsulated in an OR statement, the syntax is `col.eq.val` instead of `col=eq.val`
+        op = "." if in_or else "="
         if normalized_comparator == "sort":
+            if in_or:
+                raise ValueError(f"Argument `{argument}` can't be set in an OR group")
             return f"order={column}.{value}", True
         elif normalized_comparator == "exact":
-            return f"{column}=eq.{value}", False
+            return f"{column}{op}eq.{value}", False
         elif normalized_comparator == "differs":
-            return f"{column}=isdistinct.{value}", False
+            return f"{column}{op}isdistinct.{value}", False
         elif normalized_comparator == "isnull":
-            return f"{column}=is.null", False
+            return f"{column}{op}is.null", False
         elif normalized_comparator == "isnotnull":
-            return f"{column}=not.is.null", False
+            return f"{column}{op}not.is.null", False
         elif normalized_comparator == "contains":
-            return f"{column}=ilike.*{value}*", False
+            return f"{column}{op}ilike.*{value}*", False
         elif normalized_comparator == "notcontains":
-            return f"{column}=not.ilike.*{value}*", False
+            return f"{column}{op}not.ilike.*{value}*", False
         elif normalized_comparator == "in":
-            return f"{column}=in.({value})", False
+            return f"{column}{op}in.({value})", False
         elif normalized_comparator == "notin":
-            return f"{column}=not.in.({value})", False
+            return f"{column}{op}not.in.({value})", False
         elif normalized_comparator == "less":
-            return f"{column}=lte.{value}", False
+            return f"{column}{op}lte.{value}", False
         elif normalized_comparator == "greater":
-            return f"{column}=gte.{value}", False
+            return f"{column}{op}gte.{value}", False
         elif normalized_comparator == "strictly_less":
-            return f"{column}=lt.{value}", False
+            return f"{column}{op}lt.{value}", False
         elif normalized_comparator == "strictly_greater":
-            return f"{column}=gt.{value}", False
+            return f"{column}{op}gt.{value}", False
     raise ValueError(f"argument '{argument}={value}' could not be parsed")
 
 
