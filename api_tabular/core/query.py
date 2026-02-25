5d2399c1ad535b02680db3ab93710c4610165abc
Pierlou <pierlou.ramade@data.gouv.fr>
2026-02-25 18:26:51 +0100

    feat: allow nested conditionnal queries

--- api_tabular/core/query.py
+++ api_tabular/core/query.py
@@ -1,3 +1,4 @@
+import re
 from collections import defaultdict
 
 from api_tabular.core.utils import is_aggregation_allowed
@@ -14,6 +15,10 @@ def build_sql_query_string(
     aggregators = defaultdict(list)
     sorted = False
     for arg in request_arg:
+        if arg.startswith("or=("):
+            # top level "and=(...)" should not happen
+            sql_query.append(parse_operator(arg))
+            continue
         _split = arg.split("=")
         # filters are expected to have the syntax `<column_name>__<operator>=<value>`
         if len(_split) == 2:
@@ -72,39 +77,52 @@ def get_column_and_operator(argument: str) -> tuple[str, str]:
     return column, normalized_comparator
 
 
-def add_filter(argument: str, value: str | None) -> tuple[str | None, bool]:
+def add_filter(
+    argument: str,
+    value: str | None,
+    *,
+    in_operator: bool = False,
+) -> tuple[str | None, bool]:
     if argument in ["page", "page_size"]:  # processed differently
+        if in_operator:
+            raise ValueError(f"Argument `{argument}` can't be set in an OR group")
         return None, False
     if argument == "columns":
+        if in_operator:
+            raise ValueError(f"Argument `{argument}` can't be set in an OR group")
         return f"select={value}", False
     if "__" in argument:
         column, normalized_comparator = get_column_and_operator(argument)
+        # when encapsulated in an OR statement, the syntax is `col.eq.val` instead of `col=eq.val`
+        op = "." if in_operator else "="
         if normalized_comparator == "sort":
+            if in_operator:
+                raise ValueError(f"Argument `{argument}` can't be set in an OR group")
             return f"order={column}.{value}", True
         elif normalized_comparator == "exact":
-            return f"{column}=eq.{value}", False
+            return f"{column}{op}eq.{value}", False
         elif normalized_comparator == "differs":
-            return f"{column}=isdistinct.{value}", False
+            return f"{column}{op}isdistinct.{value}", False
         elif normalized_comparator == "isnull":
-            return f"{column}=is.null", False
+            return f"{column}{op}is.null", False
         elif normalized_comparator == "isnotnull":
-            return f"{column}=not.is.null", False
+            return f"{column}{op}not.is.null", False
         elif normalized_comparator == "contains":
-            return f"{column}=ilike.*{value}*", False
+            return f"{column}{op}ilike.*{value}*", False
         elif normalized_comparator == "notcontains":
-            return f"{column}=not.ilike.*{value}*", False
+            return f"{column}{op}not.ilike.*{value}*", False
         elif normalized_comparator == "in":
-            return f"{column}=in.({value})", False
+            return f"{column}{op}in.({value})", False
         elif normalized_comparator == "notin":
-            return f"{column}=not.in.({value})", False
+            return f"{column}{op}not.in.({value})", False
         elif normalized_comparator == "less":
-            return f"{column}=lte.{value}", False
+            return f"{column}{op}lte.{value}", False
         elif normalized_comparator == "greater":
-            return f"{column}=gte.{value}", False
+            return f"{column}{op}gte.{value}", False
         elif normalized_comparator == "strictly_less":
-            return f"{column}=lt.{value}", False
+            return f"{column}{op}lt.{value}", False
         elif normalized_comparator == "strictly_greater":
-            return f"{column}=gt.{value}", False
+            return f"{column}{op}gt.{value}", False
     raise ValueError(f"argument '{argument}={value}' could not be parsed")
 
 
@@ -116,6 +134,54 @@ def add_aggregator(argument: str, indexes: set | None) -> tuple[str, str]:
     if operator in ["avg", "count", "max", "min", "sum", "groupby"]:
         return column, operator
     raise ValueError(f"argument '{argument}' could not be parsed")
+
+
+def split_top_level(s: str) -> list[str]:
+    # we can't .split(",") as there may be commas within the params (if nested)
+    # so we need a custom "split by ',' if ',' not within parentheses"
+    parts = []
+    current = ""
+    depth = 0
+    for char in s:
+        if char == "(":
+            depth += 1
+            current += char
+        elif char == ")":
+            depth -= 1
+            current += char
+        elif char == "," and depth == 0:
+            parts.append(current)
+            current = ""
+        else:
+            current += char
+    if current:
+        parts.append(current)
+    return parts
+
+
+def parse_operator(query: str, operator: str = "or"):
+    if not query.endswith(")"):
+        raise ValueError(f"argument '{query}' could not be parsed")
+    postgrest_params = []
+    # we can safely assume that there will be one result for the regex
+    params = split_top_level(re.findall(rf"^{operator}=\((.*)\)$", query)[0])
+    for param in params:
+        if param.startswith(("and=(", "or=(")):
+            # recursively adding the nested confitions
+            postgrest_params.append(parse_operator(param, param.split("=")[0]))
+        else:
+            _split = param.split(".")
+            if len(_split) == 1:
+                # handling is(not)null
+                if _split[0].split("__")[1] in ["isnull", "isnotnull"]:
+                    postgrest_params.append(add_filter(_split[0], None, in_operator=True)[0])
+                else:
+                    raise ValueError(f"argument '{param}' could not be parsed")
+            elif len(_split) == 2:
+                postgrest_params.append(add_filter(*_split, in_operator=True)[0])
+            else:
+                raise ValueError(f"argument '{param}' could not be parsed")
+    return f"{operator}=({','.join(postgrest_params)})"
 
 
 def raise_if_not_index(column_name: str, indexes: set | None) -> None:
