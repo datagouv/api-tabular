b0d656f3f909563f4087edbfa3399215ce39a4ae
Adrien Carpentier <me@adriencarpentier.com>
2026-06-11 17:29:37 +0200

    fix(lint): fix linting issues

--- tests/test_api.py
+++ tests/test_api.py
@@ -393,9 +393,9 @@ async def test_health(client, rmock, params):
 
 
 async def test_aggregation_exceptions(client):
-    res = await client.get(f"/api/aggregation-exceptions/")
+    res = await client.get("/api/aggregation-exceptions/")
     aggregations = await res.json()
-    assert aggregations["allowed"] == False
+    assert not aggregations["allowed"]
     assert aggregations["exceptions"] == config.ALLOW_AGGREGATION_EXCEPTIONS
 
 
